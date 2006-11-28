808550abae972e3008666a4dc3bcfee3b6b72801
Paul Miller <jettero@voltar.org>
2006-11-28 10:16:10 -0500

    reversioned the package in preparation for launch.

--- PBC.pm
+++ PBC.pm
@@ -634,7 +634,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw( ) ] ); 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw( );
-our $VERSION = '0.4.1-0.5.04';
+our $VERSION = '0.4.2-0.7.15';
 
 sub AUTOLOAD {
     my $constname;
