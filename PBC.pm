b7bc5a3cca4acff48ba5eca78afff335b79d220f
Paul Miller <jettero@voltar.org>
2006-11-28 10:13:58 -0500

    added the slamtest

--- PBC.pm
+++ PBC.pm
@@ -634,7 +634,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw( ) ] ); 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw( );
-our $VERSION = '0.4.1-0.5.03';
+our $VERSION = '0.4.1-0.5.04';
 
 sub AUTOLOAD {
     my $constname;
