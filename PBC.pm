c01942f2e041ede98143e14cbff8a1e3421cd606
Paul Miller <jettero@voltar.org>
2007-02-03 11:04:27 -0500

    OK, I'm pushing out the 0.4.6 version.  The 0.4.5 version was never published because of problems discovered in my t/ dir!!
    Hurray!!

--- PBC.pm
+++ PBC.pm
@@ -665,7 +665,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw( ) ] ); 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw( );
-our $VERSION = '0.7.20.0-0.4.4';
+our $VERSION = '0.7.20.0-0.4.6';
 
 sub AUTOLOAD {
     my $constname;
