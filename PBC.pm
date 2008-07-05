4c6271c51a6a0922509c9d3266f80a0d22ad746d
Paul Miller <paul@voltar.org>
2008-07-05 06:31:57 -0400

    this warning spam is better elucidated by ./go and ./cq

--- PBC.pm
+++ PBC.pm
@@ -130,9 +130,9 @@ sub set_to_hash {
 
     croak "provide something to set the element to" unless defined $hash and length $hash > 0;
     my $type = $tt{$$this}{t};
-    warn " >type=$type; hash=$hash...@_...<\n"; 
+  # warn " >type=$type; hash=$hash...@_...<\n"; 
     &Crypt::PBC::element_from_hash($this, $hash);
-    warn " <type=$type; hash=$hash...@_...>\n"; 
+  # warn " <type=$type; hash=$hash...@_...>\n"; 
 
     $this;
 }
