555ef44c3be6cfe8143c0eba58d0fe577ab35e38
Paul Miller <paul@voltar.org>
2008-08-12 07:31:46 -0400

    versioned up

--- PBC.pm
+++ PBC.pm
@@ -670,7 +670,7 @@ use base 'Exporter';
 our %EXPORT_TAGS = ( 'all' => [ qw( ) ] ); 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw( );
-our $VERSION = 0.85;
+our $VERSION = 0.851;
 
 sub AUTOLOAD {
     my $constname;
